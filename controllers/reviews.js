6f8261cae3fa89c566fd28fcc82f319553dc2d92
sanjana <sp2529427@gmail.com>
2024-02-07 21:34:31 +0530

    Add project files

---
+++ controllers/reviews.js
@@ -0,0 +1,25 @@
+const Listing=require("../models/listing");
+const Review=require("../models/review");
+
+
+module.exports.createReview=async (req, res) => {
+    console.log(req.params.id);
+    let listing = await Listing.findById(req.params.id);
+    let newReview = new Review(req.body.review);
+    newReview.author=req.user._id;
+    listing.reviews.push(newReview);
+
+    await newReview.save();
+    await listing.save();
+    req.flash("success","New Review Created!");
+
+    res.redirect(`/listings/${listing._id}`);
+};
+module.exports.destroyReview=async(req,res)=>{
+    const { id, reviewId } = req.params; // Extracting id and reviewId from request params
+    await Listing.findByIdAndUpdate(id,{$pull:{reviews: reviewId}});
+    await Review.findByIdAndDelete(reviewId); // Corrected variable name
+    req.flash("success"," Review Deleted!");
+
+    res.redirect(`/listings/${id}`);
+};
---
+++ middleware.js
@@ -0,0 +1,63 @@
+const Listing = require("./models/listing");
+const Review = require("./models/review");
+const { listingSchema, reviewSchema } = require("./schema.js");
+const ExpressError = require("./utils/ExpressError.js");
+
+
+module.exports.isLoggedIn = (req, res, next) => {
+    if (!req.isAuthenticated()) {
+        req.session.redirectUrl = req.originalUrl;
+        req.flash("error", "You must be logged in to create a listing!");
+        return res.redirect("/login");
+    }
+    next();
+};
+
+module.exports.saveRedirectUrl = (req, res, next) => {
+    if (req.session.redirectUrl) {
+        res.locals.redirectUrl = req.session.redirectUrl;
+    }
+    next();
+};
+
+module.exports.isOwner = async (req, res, next) => {
+    let { id } = req.params;
+    let listing = await Listing.findById(id);
+    if (!listing.owner.equals(res.locals.currUser._id)) {
+        req.flash("error", "You are not the owner of this listing");
+        return res.redirect(`/listings/${id}`);
+    }
+    next();
+};
+
+// Define validateListing function
+module.exports.validateListing = (req, res, next) => {
+    let { error } = listingSchema.validate(req.body);
+    if (error) {
+        let errMsg = error.details.map((el) => el.message).join(",");
+        throw new ExpressError(400, errMsg);
+    } else {
+        next();
+    }
+};
+
+module.exports.validateReview = (req, res, next) => {
+    let { error } = reviewSchema.validate(req.body);
+    if (error) {
+        let errMsg = error.details.map((el) => el.message).join(",");
+        throw new ExpressError(400, errMsg);
+    } else {
+        next();
+    }
+};
+
+
+module.exports.isReviewAuthor = async (req, res, next) => {
+    let { id,reviewId } = req.params;
+    let review = await Review.findById(reviewId);
+    if (!review.author.equals(res.locals.currUser._id)) {
+        req.flash("error", "You are not the author of this listing");
+        return res.redirect(`/listings/${id}`);
+    }
+    next();
+};
